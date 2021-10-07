1b77b012f5b2d341ea629a36636dbc37fbfcc18c
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-06 21:53:49 -0300

    add package device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
-main() => runApp(MyApp());
+main() => runApp(DevicePreview(builder: (_)=>MyApp()));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      locale: DevicePreview.locale(context),
       debugShowCheckedModeBanner: false,
       title: 'App Responsivo',
       home: Home(),
